d77b01242bc15af212c8a7cfff2de3a2c979c868
douglas.vieira <douglasbatat@gmail.com>
2025-12-01 09:06:09 -0300

    Commit inicial.

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Pessoa {
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Principal {
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    Pessoa pessoa1 = new Pessoa("Douglas", 39);
+    Pessoa pessoa2 = new Pessoa("Talita", 35);
+    Pessoa pessoa3 = new Pessoa("Ana Luiza", 6);
+
+    ArrayList<Pessoa> listaDePessoa = new ArrayList<>();
+    listaDePessoa.add(pessoa1);
+    listaDePessoa.add(pessoa2);
+    listaDePessoa.add(pessoa3);
+
+    System.out.println(listaDePessoa.size());
+    System.out.println(listaDePessoa.get(0));
+    System.out.println(listaDePessoa);
+
+    }
 }
