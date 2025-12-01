8434da72b8212314c043cc6bcaf2dda97cba8cb7
douglas.vieira <douglasbatat@gmail.com>
2025-12-01 13:20:45 -0300

    Commit após ajustes com IA.

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
     public static void main(String[] args) {
@@ -15,6 +16,20 @@ public class Principal {
     System.out.println(listaDePessoa.size());
     System.out.println(listaDePessoa.get(0));
     System.out.println(listaDePessoa);
+
+    //Código com a revisão da IA
+
+    var pessoa4 = new Pessoa("João", 28);
+    var pessoa5 = new Pessoa("Maria", 32);
+    var pessoa6 = new Pessoa("Pedro", 25);
+
+    List<Pessoa> pessoas = new ArrayList<>(List.of(pessoa4, pessoa5, pessoa6));
+
+    System.out.println("Número de pessoas na lista: " + pessoas.size());
+    System.out.println("Primeira pessoa na lista: " + pessoas.get(0));
+    pessoas.forEach(System.out::println);
+
+
 
     }
 }
